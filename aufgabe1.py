0fae738e37559117e651325f73421ba772e26746
Michael <michael.fondue@gmail.com>
2017-04-25 22:30:27 +0200

    aufgabe1 erweitert mit Ideen

--- aufgabe1.py
+++ aufgabe1.py
@@ -16,8 +16,19 @@ def getfreqwords(indir, outfile):
 	
 	sacbooks = []
 	sacbooks = glob.glob(indir + 'SAC-Jahrbuch_*.xml', recursive=False)
-	print(sacbooks)
-
+	
+	for file in sacbooks:
+		#with open(file) as src, open('output.txt', 'w') as trg:
+		sentences = []
+		tree = etree.parse(file)
+		sentences.append(file.get('s')
+			
+			#sentence_hashes = set()
+			#for sentence in src:
+				#sentence_hash = hash(sentence)
+				#if sentence_hash not in sentence_hashes:
+				#	sentence_hashes.add(sentence_hash)
+				#	trg.write(sentence)
 
 	outputfile = open(outfile , 'w')
 	for x in sacbooks:
@@ -29,8 +40,12 @@ def getfreqwords(indir, outfile):
 
 def main():
 
-	getfreqwords('x', 'bla.txt')	
+	getfreqwords('SAC/', 'output.txt')	
 
+
+
+if __name__ == '__main__':
+    main()
 
 
 if __name__ == '__main__':
