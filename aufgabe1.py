61237cd39fe38f25d2d4d354f9ebbd20f7d965a6
Michael <michael.fondue@gmail.com>
2017-05-01 22:25:09 +0200

    neue version mit xml zerlegt

--- aufgabe1.py
+++ aufgabe1.py
@@ -7,33 +7,77 @@
 
 import io
 import sys
-import xml.etree.ElementTree as ET
 import json
 import glob
-from lxml import etree
+import string
+
+#from lxml import etree as ET
+from lxml import etree as ET
 
 def getfreqwords(indir, outfile):
 	sentences_count = {}
 	sacbooks = []
-	sacbooks = glob.glob(indir + 'SAC-Jahrbuch_*.xml', recursive=False)
+	element = []
+	words = []
+	sent = []
 	
-	for file in sacbooks:
-		#with open(file) as src, open('output.txt', 'w') as trg:
-		sentences = []
-		tree = etree.parse(file)
-		sentences.append(hash(tree.findall('<s*<\\s>')))
+	sacbooks = glob.glob(indir + 'SAC-Jahrbuch_*.xml', recursive=False)
+	#print(sacbooks)
+	#test = open(sacbooks[0], 'r')
+	#print(test.read())
+	#test.close()
+	tree = ET.parse(sacbooks[0])
+	root = tree.getroot()
+	for sentence in root.findall('.//s'):
+		element.append(ET.tostring(sentence, method = 'text', pretty_print = True, encoding='utf-8'))
+
+		#print(ET.tostring(sentence, method = 'text', pretty_print = True, encoding='utf-8'))
+	 	#print('nächster Satz')
+		for word in element:
+			strword = str(word, 'utf-8')
+			words.append(strword.split())
+	 		#print(ET.tostring(word, method = 'text', encoding='utf-8'))
+	
+	for sent in words:
+		for x in string.punctuation:
+			if (x in sent):
+				sent.remove(x)
+		if (len(sent) >= 6):
+			print(set(sent))
+	#print(words)
+	#print(element[0])
+	#print(ET.tostring(sentencelist[0]))
+	#print(ET.tostring(root, pretty_print=True))
+
+	# for file in sacbooks:
+	# 	#with open(file) as src, open('output.'txt', 'w') as trg:
+	# 	sentence_hash = set()
+	# 	tree = ET.parse(sacbooks[0])
+	# 	root = tree.getroot()
+	# 	print(root.get('lemma'))
+
+	# 	for word in root:
+			#print(ET.tostring(word, encoding='utf8', method='xml'))
+		#print(tree.findall('<s*>'))
+	#	sentences.append(hash(tree.findall('<s*<\\s>')))
+		#sentence_hash.append(tree.findall('<s*<\\s>'))
 		#print(sentences[0])
-	for words in sentence:
-		if(len(words.split()) >= 6):
-			if(words in sentences_count):
-				num = sentences_count[words]
-				sentences_counts[words] = num + 1
-			else:
-				sentences_count.update({words:1})
-	list = sorted(sentences_count.items())
-	for x in range(0,20):
-		print(sentenced_count[x])
-			
+
+
+	# for words in sentence_hash:
+	# 	#if(len(words.split()) >= 6):
+	# 	if(words in sentences_count):
+	# 		num = sentences_count[words]
+	# 		sentences_counts[words] = num + 1
+	# 	else:
+	# 		sentences_count.update({words:1})
+	# list = sorted(sentences_count.items())
+	# for x in range(0,20):
+	# 	print(sentenced_count[x])
+		
+
+
+
 			#sentence_hashes = set()
 			#for sentence in src:
 				#sentence_hash = hash(sentence)
